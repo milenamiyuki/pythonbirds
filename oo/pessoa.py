84c72761dbf031e30fe25bafec926922338e4dea
Milena <milenatatekawa@yahoo.com.br>
2020-09-02 19:43:02 -0300

    Criados atributos de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,15 @@
 class Pessoa:
+    def __init__(self,nome=None):
+        self.nome = nome
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Barbara')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Milena'
+    print(p.nome)
+
